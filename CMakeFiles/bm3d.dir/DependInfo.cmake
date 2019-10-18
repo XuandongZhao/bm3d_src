090c69867abebd924e2cd489d1fd92124985e2cf
XuandongZhao <csxuandongzhao@gmail.com>
2019-10-18 12:13:22 -0700

    cmake success

---
+++ CMakeFiles/bm3d.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/xuandongz/bm3d_src/io_png.c" "/home/xuandongz/bm3d_src/CMakeFiles/bm3d.dir/io_png.o"
+  "/home/xuandongz/bm3d_src/mt19937ar.c" "/home/xuandongz/bm3d_src/CMakeFiles/bm3d.dir/mt19937ar.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "PUBLIC"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xuandongz/bm3d_src/bm3d.cpp" "/home/xuandongz/bm3d_src/CMakeFiles/bm3d.dir/bm3d.o"
+  "/home/xuandongz/bm3d_src/lib_transforms.cpp" "/home/xuandongz/bm3d_src/CMakeFiles/bm3d.dir/lib_transforms.o"
+  "/home/xuandongz/bm3d_src/main.cpp" "/home/xuandongz/bm3d_src/CMakeFiles/bm3d.dir/main.o"
+  "/home/xuandongz/bm3d_src/utilities.cpp" "/home/xuandongz/bm3d_src/CMakeFiles/bm3d.dir/utilities.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "PUBLIC"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/bm3d.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bm3d.dir/bm3d.o"
+  "CMakeFiles/bm3d.dir/io_png.o"
+  "CMakeFiles/bm3d.dir/lib_transforms.o"
+  "CMakeFiles/bm3d.dir/mt19937ar.o"
+  "CMakeFiles/bm3d.dir/utilities.o"
+  "CMakeFiles/bm3d.dir/main.o"
+  "bm3d.pdb"
+  "bm3d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/bm3d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
